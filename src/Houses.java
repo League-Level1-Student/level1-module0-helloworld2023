b7e2e959606d4344ecf56dff5f2d5dd611477fc8
helloworld2023 <league@wts13.attlocal.net>
2019-01-26 11:24:27 -0800

    Started Level 1 Module 0

---
+++ src/Houses.java
@@ -0,0 +1,16 @@
+import org.jointheleague.graphical.robot.Robot;
+
+public class Houses {
+
+	public static void main(String[] args) {
+		
+		Robot idk = new Robot();
+		idk.setSpeed(30);
+		idk.turn(270);
+		idk.move(450);
+		idk.turn(270);
+		idk.move(300);
+		idk.turn(270);
+	}
+	
+}
